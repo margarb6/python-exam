b457b99da173bc407375ae97495011cc5a5dd4fc
Marta <margarb6@epsg.upv.es>
2021-03-24 10:56:21 +0100

    checkpoint 2

--- examen.py
+++ examen.py
@@ -1,16 +1,27 @@
 def get_list(doc):
     f = open(doc, mode="rt", encoding="utf-8")
     lista_lineas = f.readlines()
-    palabras= []
+    lista_palabras = split_lineas(lista_lineas)
+    dic_palabras = {}
+    for palabra in lista_palabras:
+        for i in palabra:
+            if dic_palabras.get(len(i)) != i:
+                if dic_palabras.get(len(i)) != None:
+                    aux = dic_palabras.get(len(i))
+                    aux = str(aux) + ", " + i
+                else:
+                    dic_palabras[len(i)] = i
+                    print(dic_palabras)
       
         
 
 
         
 
+def split_lineas(lineas):
+    palabras = []
+    for linea in lineas:
+        palabras.append(linea.split(" "))
+        return palabras
 
-def split_lineas()
-    for linea in lista_lineas:
-        palabras.append(linea.split(" ")
-        
 get_list("doc_pruebas.txt")
