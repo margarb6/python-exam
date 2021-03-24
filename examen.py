cef9c71a4278758d7c91140b9b39b2c8c39f8fc3
Marta <margarb6@epsg.upv.es>
2021-03-24 10:35:48 +0100

    checkpoint 1

---
+++ examen.py
@@ -0,0 +1,16 @@
+def get_list(doc):
+    f = open(doc, mode="rt", encoding="utf-8")
+    lista_lineas = f.readlines()
+    palabras= []
+      
+        
+
+
+        
+
+
+def split_lineas()
+    for linea in lista_lineas:
+        palabras.append(linea.split(" ")
+        
+get_list("doc_pruebas.txt")
